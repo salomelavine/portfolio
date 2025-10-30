7c3f63823dab278a28779a2c152e354b0a39aaea
Salome Lavine <salome.lavine@epfl.com>
2025-10-30 11:13:33 +0100

    Updated about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Education from '../components/Education';
 import Experience from '../components/Experience';
 import Projects from '../components/Projects';
 import Skills from '../components/Skills';
-import Contact from '../components/Contact';
+// import Contact from '../components/Contact';
 import Intrest from'../components/Interests';
 import Footer from '../components/Footer';
 
@@ -23,7 +23,7 @@ export default function Home() {
         <Experience />
         <Projects />
         <Skills />
-        <Contact />
+        {/* <Contact /> */}
       </main>
       <Footer />
     </>
