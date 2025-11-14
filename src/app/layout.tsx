ae9671de3e1412d30f1c162858e2b4ed783cb83a
Salome Lavine <salome.lavine@epfl.com>
2025-11-14 09:50:08 +0100

    Updated about section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
